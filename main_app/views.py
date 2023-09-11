13d9ee1947cfb04f1abb00fd28a3749a86aab01c
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-09-11 18:58:27 -0500

    define url and view for about and chicken index. add css styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+chickens = [
+    {'name': 'Nugget', 'breed': 'Buff Orpington', 'description': 'very friendly, loves mealworms', 'age': 3},
+    {'name': 'Colonel', 'breed': 'Rhode Island Red', 'description': 'fiesty, likes to peck at any visitors', 'age': 3}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def chickens_index(request):
+    return render(request, 'chickens/index.html', {
+        'chickens': chickens
+    })
